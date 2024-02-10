e34ae9b88a0a9976a23f48b2cbdce98d8b2c3cc4
DanDias00 <dandiasabeyesinghe@gmail.com>
2024-02-10 14:12:00 +0530

    Added email functionality when registering

---
+++ application/config/email_config.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// Email configuration
+$config['email_settings'] = array(
+    'protocol'  => 'smtp',
+    'smtp_host' => 'smtp.gmail.com',
+    'smtp_user' => 'dandiasabeyesinghe@gmail.com',
+    'smtp_pass' => 'fsfr kiut xiet nmmm',
+    'smtp_crypto' => 'tls',
+    'smtp_port' => 587,
+    'mailtype'  => 'html',
+    'charset'   => 'utf-8',
+    'newline'   => "\r\n"
+);
