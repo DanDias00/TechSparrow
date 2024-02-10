0010d289f319ecad1cd2b9953e55c9bde5bb929a
DanDias00 <dandiasabeyesinghe@gmail.com>
2024-02-10 11:24:52 +0530

    Added base home page with header and footer

---
+++ application/controllers/Home.php
@@ -0,0 +1,28 @@
+<?php
+class Home extends CI_Controller{
+
+
+public function __construct() {
+        parent::__construct();
+        $this->load->library('form_validation');
+        $this->load->library('session');
+        $this->load->helper('url');
+       
+}
+    public function view($page = 'home')
+{
+        if ( ! file_exists(APPPATH.'views/'.$page.'.php'))
+        {
+                
+                show_404();
+        }
+
+        
+
+        $this->load->view('templates/header');
+        $this->load->view($page);
+        $this->load->view('templates/footer');
+}
+
+}
+?>
---
+++ application/views/templates/header.php
@@ -0,0 +1,8 @@
+<html>
+        <head>
+                <title>CodeIgniter Tutorial</title>
+        </head>
+        <body>
+
+            
+                <h2>this is header</h2>
