491b43083975f1f07845490b812d05bb4262443b
DanDias00 <dandiasabeyesinghe@gmail.com>
2024-02-13 10:22:13 +0530

    Added login UI and fuctionality

--- application/views/users/login.php
+++ application/views/users/login.php
@@ -6,39 +6,58 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Login</title>
-    <link rel="stylesheet" href="path/to/your/css/bootstrap.min.css"> <!-- Bootstrap for styling, if you're using it -->
+    <title>Log in - Tech Sparrow</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
     <style>
-        .container {
-            margin-top: 50px;
+        .auth-wrapper {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        .auth-inner {
+            width: 300px;
+        }
+        .social-login {
+            margin-bottom: 1rem;
         }
     </style>
 </head>
 <body>
 
-<div class="container">
-    <h2>Login</h2>
-    <?php if($this->session->flashdata('login_failed')): ?>
-    <div class="alert alert-danger">
-        <?php echo $this->session->flashdata('login_failed'); ?>
-    </div>
-    <?php endif; ?>
+<div class="auth-wrapper">
+        <div class="auth-inner">
+             <h2>Login</h2>
+                <?php if($this->session->flashdata('login_failed')): ?>
+                <div class="alert alert-danger">
+                    <?php echo $this->session->flashdata('login_failed'); ?>
+                </div>
+                <?php endif; ?>
 
-    <?php echo form_open('User/login'); ?>
-    
-    <div class="form-group">
-        <label for="username">Username:</label>
-        <input type="text" class="form-control" name="username" id="username" required>
-    </div>
+                <?php echo form_open('User/login'); ?>
+                
+                <div class="form-group">
+                    <label for="username">Username:</label>
+                    <input type="text" class="form-control" name="username" id="username" placeholder="Enter your username" required>
+                </div>
 
-    <div class="form-group">
-        <label for="password">Password:</label>
-        <input type="password" class="form-control" name="password" id="password" required>
-    </div>
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" class="form-control" name="password" id="password" placeholder="Enter your password " required>
+                </div>
 
-    <button type="submit" class="btn btn-primary">Login</button>
+                <!-- Login Button -->
+                <button type="submit" class="btn btn-primary btn-block">Log in</button>
 
-    <?php echo form_close(); ?>
+                <!-- Additional Links -->
+                <p class="forgot-password text-right">
+                    Forgot <a href="#">password?</a>
+                </p>
+                <p class="text-right">
+                    Don't have an account? <a href="#">Sign up</a>
+                </p>
+
+                <?php echo form_close(); ?>
 </div>
 
 <script src="path/to/your/js/jquery.min.js"></script>
